683bf8caa676ac14698a919cd4218a54d7d99d4f
CharliePayne1 <charliepayne1101@gmail.com>
2020-06-13 20:04:34 +0100

    Done.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ document.addEventListener('DOMContentLoaded', () => {
 // ----------------------------------------------------------------------------------------
 
 const ul = document.getElementById('quote-list');
+const orderButton = document.createElement('button');
+orderButton.innerText = "Order Alphabetically";
+const idButton = document.createElement('button');
+idButton.innerText = "Order Chronologically"
+ul.append(orderButton)
+
 // ----------------------------------------------------------------------------------------
 // Fetch all the quotes 
 // ----------------------------------------------------------------------------------------
@@ -11,7 +17,7 @@ const getQuotes = () => {
 fetch('http://localhost:3000/quotes?_embed=likes')
 .then(resp => resp.json())
 .then(quotes => renderQuotes(quotes))
-// .catch(error => console.log(error.message));
+.catch(error => console.log(error.message));
 }
 
 const renderQuotes = (quotes) => {
@@ -112,9 +118,11 @@ fetch(`http://localhost:3000/quotes/${quote.id}`, configObject)
 // ----------------------------------------------------------------------------------------
 // Add a quote  
 // ----------------------------------------------------------------------------------------
+
 const like = (quote) => {
 const object = {
 quoteId: quote.id,
+created_at: Date.now()
 };
 
 const configObject = {
@@ -127,8 +135,58 @@ body: JSON.stringify(object)
 };
 
 fetch('http://localhost:3000/likes', configObject)
-.then(resp => resp.json())
-.then(getQuotes())
+.then(resp => resp.json());
+}
+
+// ----------------------------------------------------------------------------------------
+// Toggle an alphabet button  
+// ----------------------------------------------------------------------------------------
+
+orderButton.addEventListener('click', e => {
+    e.preventDefault();
+    fetch('http://localhost:3000/quotes?_sort=author')
+    .then(resp => resp.json())
+    .then(quotes => renderQuotesAlphabetically(quotes))
+    // .catch(error => console.log(error.message));
+});
+
+const renderQuotesAlphabetically = (quotes) => {
+    const oldQuotes = document.querySelectorAll(".quote-card")
+    oldQuotes.forEach(oldQuote => oldQuote.remove())
+    quotes.forEach(quote => {
+        renderQuoteAlphabetically(quote)
+    });
+}
+
+const renderQuoteAlphabetically = (quote) => {
+    const li = document.createElement('li');
+    li.className = "quote-card";
+
+    const blockquote = document.createElement('blockquote');
+    blockquote.className = "blockquote";
+
+    const p =document.createElement('p');
+    p.className = "mb-0";
+    p.innerText = quote.quote
+
+    const footer = document.createElement('footer');
+    footer.className = "blockquote-footer";
+    footer.innerText = quote.author;
+
+    const br = document.createElement('br');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn-danger';
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener('click', e => {
+        e.preventDefault();
+        deleteQuote(quote);
+        e.target.parentNode.parentNode.remove()
+    });
+
+    blockquote.append(p, footer, br, deleteButton);
+    li.append(blockquote);
+    ul.append(li)
 }
 
 getQuotes()
