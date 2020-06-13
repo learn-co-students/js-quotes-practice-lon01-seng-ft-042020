c0040a83878ce6429d3366466da85ba2a1a38816
CharliePayne1 <charliepayne1101@gmail.com>
2020-06-13 12:19:39 +0100

    Done.

---
+++ src/index.js
@@ -0,0 +1,135 @@
+document.addEventListener('DOMContentLoaded', () => {
+// ----------------------------------------------------------------------------------------
+// Consts 
+// ----------------------------------------------------------------------------------------
+
+const ul = document.getElementById('quote-list');
+// ----------------------------------------------------------------------------------------
+// Fetch all the quotes 
+// ----------------------------------------------------------------------------------------
+const getQuotes = () => {
+fetch('http://localhost:3000/quotes?_embed=likes')
+.then(resp => resp.json())
+.then(quotes => renderQuotes(quotes))
+// .catch(error => console.log(error.message));
+}
+
+const renderQuotes = (quotes) => {
+    quotes.forEach(quote => {
+        renderQuote(quote)
+    });
+}
+
+const renderQuote = (quote) => {
+    const li = document.createElement('li');
+    li.className = "quote-card";
+
+    const blockquote = document.createElement('blockquote');
+    blockquote.className = "blockquote";
+
+    const p =document.createElement('p');
+    p.className = "mb-0";
+    p.innerText = quote.quote
+
+    const footer = document.createElement('footer');
+    footer.className = "blockquote-footer";
+    footer.innerText = quote.author;
+
+    const br = document.createElement('br');
+
+    const likeButton = document.createElement('button');
+    likeButton.className = 'btn-success';
+    likeButton.innerHTML = `Likes: <span>${quote.likes.length}</span>`;
+    likeButton.addEventListener('click', e => {
+        like(quote);
+        likeButton.innerHTML = `Likes: <span>${quote.likes.length += 1}</span>`;
+    });
+
+    const deleteButton = document.createElement('button');
+    deleteButton.className = 'btn-danger';
+    deleteButton.innerText = "Delete";
+    deleteButton.addEventListener('click', e => {
+        e.preventDefault();
+        deleteQuote(quote);
+        e.target.parentNode.parentNode.remove()
+    });
+
+    blockquote.append(p, footer, br, likeButton, deleteButton);
+    li.append(blockquote);
+    ul.append(li)
+}
+
+// ----------------------------------------------------------------------------------------
+// Add a quote  
+// ----------------------------------------------------------------------------------------
+
+const form = document.querySelector('#new-quote-form');
+form.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const object = {
+            quote: form[0].value,
+            author: form[1].value,
+            likes: []
+        };
+
+        const configObject = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+        },
+        body: JSON.stringify(object)
+        };
+
+        fetch('http://localhost:3000/quotes', configObject)
+        .then(resp => resp.json())
+        .then(quote => renderQuote(quote))
+
+    });
+
+// ----------------------------------------------------------------------------------------
+// Delete a quote 
+// ----------------------------------------------------------------------------------------
+const deleteQuote = (quote) => {
+    
+const configObject = {
+method: 'DELETE',
+headers: {
+'Content-Type': 'application/json',
+Accept: 'application/json'
+},
+body: JSON.stringify({
+    id: quote.id,
+    quote: quote.quote,
+    author: quote.author
+})
+};
+
+fetch(`http://localhost:3000/quotes/${quote.id}`, configObject)
+}
+
+// ----------------------------------------------------------------------------------------
+// Add a quote  
+// ----------------------------------------------------------------------------------------
+const like = (quote) => {
+const object = {
+quoteId: quote.id,
+};
+
+const configObject = {
+method: 'POST',
+headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json'
+},
+body: JSON.stringify(object)
+};
+
+fetch('http://localhost:3000/likes', configObject)
+.then(resp => resp.json())
+.then(getQuotes())
+}
+
+getQuotes()
+})
